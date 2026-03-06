330eac10a806b91f2d653e30300c9731e8af4576
Vinicius Neri <43595413+Trollitos@users.noreply.github.com>
2026-03-06 15:50:53 +0000

    teste deploy automatico

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isso aqui ainda não tem nada, mas alguma hora vai ter</h1>
+    return <h1>Isso aqui ainda não tem nada, mas alguma hora vai ter, confia</h1>
 }
 
 export default Home;
